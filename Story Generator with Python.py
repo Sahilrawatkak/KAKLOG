679e0515af258fc32f3a9bb43985191a9c74260f
Sahil Rawat <72985947+Sahilrawatkak@users.noreply.github.com>
2021-07-05 22:31:06 +0530

    Update Story Generator with Python.py

--- Story Generator with Python.py
+++ Story Generator with Python.py
@@ -2,7 +2,7 @@
 import random
 when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
 who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
-name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
+name = ['Sahil', 'Hunny','Himanshu', 'Chandan', 'Rohit']
 residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
 went = ['cinema', 'university','seminar', 'school', 'laundry']
 happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
